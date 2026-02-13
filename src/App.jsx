6a166bc835a99b05a1447394d93d987605dcbd90
Habiba <habibanadeem075@gmail.com>
2026-02-13 02:39:12 -0800

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import Navbar from "./components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+
+import Loader from "./components/Loader";
 
 
 function App() {
+  
+   
+
   return (
     <>
-<Navbar />
 
-
+        <Loader />
+    
+        <Navbar />
+      
     </>
-  )
+  );
+  
 }
 
 export default App
